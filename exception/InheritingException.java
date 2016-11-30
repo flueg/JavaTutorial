bfb8abc11bd8775d88e0ae4366c92f18a72b6139
flueg <fluegliu@gmail.com>
2016-12-01 00:19:15 +0800

    submit a tutorial for customized exception.

---
+++ exception/InheritingException.java
@@ -0,0 +1,22 @@
+// Creating my own exception class
+import static com.liu.flueg.util.Print.*;
+
+class SimpleException extends Exception {}
+
+public class InheritingException {
+    public void f() throws SimpleException {
+        print("Throw SimpleException from f()");
+        // let the compiler build the default constructor
+        throw new SimpleException();
+    }
+
+    public static void main(String[] args) {
+        InheritingException ie = new InheritingException();
+        try {
+            ie.f();
+        } catch(SimpleException e) {
+            print("Catch SimpleException:"+e.toString());
+        }
+    }
+
+}
